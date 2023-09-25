18d92491d54d8bd277e3dd7b0e9e5c2b0547e388
zhazhazhu <fangyuan.leslie@qq.com>
2023-09-25 23:04:52 +0800

    perf: printLn to eprintLn

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = run(config) {
