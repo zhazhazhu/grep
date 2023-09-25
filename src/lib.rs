008314e4bd99ccf7366fb56d1a808c112f0ca934
zhazhazhu <fangyuan.leslie@qq.com>
2023-09-25 21:12:21 +0800

    feat: the file content can be query

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     return Ok(());
 }
 
@@ -20,5 +22,31 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
         return Ok(Config { query, filename });
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+  Rust:
+  safe, fast, productive.
+  Pick three";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
