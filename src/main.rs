cbe210ae4c0a54e5856b333197c534ef472bf671
zhazhazhu <fangyuan.leslie@qq.com>
2023-09-25 20:38:50 +0800

    refactor: Improve module and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("query is {}, In filename is {}", query, filename)
+    let config = Config::new(&args);
+    let contents = read_file(&config.filename);
+    println!("With text:\n{}", contents)
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &Vec<String>) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        return Config { query, filename };
+    }
+}
+
+fn read_file(filename: &String) -> String {
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    return contents;
 }
